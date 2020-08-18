c5f77a516e9b3bb16dddd15faec7d2daff291c45
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-08-18 14:10:06 +0300

    Add test endpoint for WebApi

---
+++ src/NetCore.BackgroundWorkerPrototype.WebApplication/Controllers/WorkerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.BackgroundWorkerPrototype.WebApplication.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    public sealed class WorkerController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/worker")]
+        public IActionResult Index()
+        {
+            return Ok("Background Worker + API");
+        }
+    }
+}
